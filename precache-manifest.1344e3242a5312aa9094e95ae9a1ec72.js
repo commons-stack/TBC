26743708b32926c222fd659e0421d79ad071c69e
dapplion <guillem.lion.benosa@gmail.com>
2019-04-09 03:09:36 +0200

    Updates

---
+++ precache-manifest.1344e3242a5312aa9094e95ae9a1ec72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "670433767e139f08c972",
+    "url": "/TBC/static/js/runtime~main.1d626f60.js"
+  },
+  {
+    "revision": "57ed03d6126f01edf032",
+    "url": "/TBC/static/js/main.123a4399.chunk.js"
+  },
+  {
+    "revision": "3d4b3c4b25a8e96e126f",
+    "url": "/TBC/static/js/2.cbdfaefe.chunk.js"
+  },
+  {
+    "revision": "57ed03d6126f01edf032",
+    "url": "/TBC/static/css/main.a3632aa3.chunk.css"
+  },
+  {
+    "revision": "fb9f942565d452e48716766130ae7295",
+    "url": "/TBC/index.html"
+  }
+];
